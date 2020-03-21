15a810181987b5aeacf74465ebebaefc0635a5c2
denso <jianyuchen@berkeley.edu>
2020-03-20 17:48:21 -0700

    update setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='e2e_percep',
+      version='0.1',
+      description='End-to-end Autonomous Driving Perception',
+      url='https://github.com/cjy1992/detect-loc-map',
+      author='Jianyu Chen',
+      author_email='jianyuchen@berkeley.edu',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
